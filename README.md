81088b4aecd1734a27e669ce6337de60477aeefc
eleanorsoft-web <hello@eleanorsoft.com>
2024-08-01 01:05:28 +0300

    refactoring and readme

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
-Page speed tool
+# Page speed tool
+This tool helps to improve page speed score by delaying loading of non-critical resources.
+
+> Note: it's always better to optimize your resources to reach the best performance without using similar tools as optimized pages create better UX.
+
+## Installation
+### Method 1
+Download and include `page-speed-tool.min.js` after opening `<head>` tag:
+```html
+<html>
+    <head>
+        <script src="/path/to/page-speed-tool.js"></script>
+        ...
+    </head>
+```
+
+Do not use `defer` or `async` in the `<script>` tag.
+
+
+### Method 2
+Copy and paste contents of the `page-speed-tool.min.js` file after opening `<head>` tag:
+```html
+<html>
+    <head>
+        <script>
+            var PageSpeedTool = new function () { ...
+        </script>
+        ...
+    </head>
+```
+
+## How it works
+All deferred resources are loaded when a user interacts with the page (scroll, click, touch, mousemove).
+
+Also it is possible to trigger loading after some timeout even if the user hasn't interacted with the page yet.
+
+And a cookie can be set in the user's browser to trigger resource loading right away on subsequent requests when resources are already cached in the browser.
+
+## Usage
+All delayed resources are loading in the same order they appear in the code. They are being loaded one by one to preserve any dependencies that still may exists between them.
+
+If you want to load delayed resources asynchronously, use low-level `afterInit()` method.
+
+### Delay styles
+```javascript
+PageSpeedTool.deferredStyle('/path/to/style.css');
+```
+
+### Delay scripts
+```javascript
+PageSpeedTool.deferredScript('/path/to/script.js');
+PageSpeedTool.deferredScript(async () => {
+    await do_something();
+    return;
+});
+PageSpeedTool.deferredScript(function () {
+    return new Promise(function (resolve) {
+        ...
+        return resolve();
+    });
+});
+```
+
+### Enable timeout
+```javascript
+PageSpeedTool.enableTimeout(); // start loading everything in 1 second
+PageSpeedTool.enableTimeout(3000); // start loading everything in 3 seconds
+PageSpeedTool.disableTimeout();
+```
+
+### Enable cookie
+Cookie TTL is 30 days.
+
+```javascript
+PageSpeedTool.enableCookie(); // use "pst_skip_delay" cookie as a marker of subsequent requests
+PageSpeedTool.enableCookie("my_custom_cookie"); // use "my_custom_cookie" cookie as a marker of subsequent requests
+```
+
+### Defer arbitrary code
+```javascript
+PageSpeedTool.afterInit(async () => {
+    do_something();
+    return;
+});
+```
--- page-speed-tool.js
+++ page-speed-tool.js
@@ -1,78 +1,13 @@
-var PageSpeedTool = new function () {
+/**
+ * page-speed-tool
+ * version 1.0.1
+ */
+var PageSpeedTool = (typeof(PageSpeedTool) !== 'undefined') ? PageSpeedTool : new function () {
 
-  let initTimeout = null;
-  let cookieName = null;
-  let afterInitInProgress = false;
-  let afterInitQueue = [];
-
-  this.afterInit = async (l) => {
-      if (this.initialized) {
-        if (afterInitInProgress) {
-            afterInitQueue.push(l);
-        } else {
-            afterInitInProgress = true;
-            await l();
-            while (afterInitQueue.length > 0) {
-                cb = afterInitInProgress.shift();
-                await cb();
-            }
-            afterInitInProgress = false;
-        }
-        return;
-      }
-      this.listeners.push(l);
-  };
-  this.onUserAction = async (l) => {
-      throw new Error('deprecated');
-      if (this.userOnlyScriptsInitialized) {
-          return await l();
-        //   return setTimeout(l, 0);
-      }
-      this.userOnlyScripts.push(l);
-  };
-  
-  this.deferredStyle = (s) => {
-      this.afterInit(function(){
-          var styles = document.createElement('link');
-          styles.rel = 'stylesheet';
-          styles.type = 'text/css';
-          styles.media = 'all';
-          styles.href = s;
-          document.getElementsByTagName('head')[0].appendChild(styles);
-      });
-  };
-  
-  this.deferredScript = (s, forUser) => {
-      const method = (forUser ? 'onUserAction' : 'afterInit');
-      this[method](async function(){
-          if (typeof(s) === 'string') {
-              await (new Promise((resolve) => {
-                  var script = document.createElement('script');
-                  script.onload = resolve;
-                  script.src = s;
-                  document.getElementsByTagName('head')[0].appendChild(script);
-              }));
-          } else {
-              await s();
-          }
-      });
-  };
-
-  this.enableTimeout = (t) => {
-    initTimeout = (t ? t : 1000);
-  };
-
-  this.disableTimeout = () => {
-    initTimeout = null;
-  }
-
-  this.enableCookie = (n) => {
-    cookieName = (n ? n : 'pst_skip_delay');
-    if (getCookie(cookieName)) {
-        initializeUserOnlyScripts();
-    }
-    setCookie(cookieName, 1, 30);
-  };
+    let initTimeout = null;
+    let cookieName = null;
+    let afterInitInProgress = false;
+    let afterInitQueue = [];
 
     function setCookie(name,value,days) {
         var expires = "";
@@ -93,34 +28,112 @@ var PageSpeedTool = new function () {
         }
         return null;
     }
-  
-  this.initialized = false;
-  this.userOnlyScriptsInitialized = false;
-  this.listeners = [];
-  this.userOnlyScripts = [];
-  var initialize = async () => {
-      if (this.initialized) { return; }
-      this.initialized = true;
-      for (let i = 0; i < this.listeners.length; i++) {
-          await this.listeners[i]();
-      }
-  }
-  var initializeUserOnlyScripts = async () => {
-      if (this.userOnlyScriptsInitialized) { return; }
-      this.userOnlyScriptsInitialized = true;
-      await initialize();
-      for (let i = 0; i < this.userOnlyScripts.length; i++) {
-          await this.userOnlyScripts[i]();
-      }
-  };
-  
-  window.addEventListener('load', () => {
-      document.addEventListener('mousemove', initializeUserOnlyScripts);
-      document.addEventListener('touchmove', initializeUserOnlyScripts);
-      document.addEventListener('click', initializeUserOnlyScripts);
 
-      if (initTimeout) {
-        setTimeout(initialize, initTimeout);
-      }
-  });
+    this.initialized = false;
+    this.userOnlyScriptsInitialized = false;
+    this.listeners = [];
+    this.userOnlyScripts = [];
+
+    var initialize = async () => {
+        if (this.initialized) { return; }
+        this.initialized = true;
+        for (let i = 0; i < this.listeners.length; i++) {
+            await this.listeners[i]();
+        }
+    }
+    var initializeUserOnlyScripts = async () => {
+        if (this.userOnlyScriptsInitialized) { return; }
+        this.userOnlyScriptsInitialized = true;
+        await initialize();
+        for (let i = 0; i < this.userOnlyScripts.length; i++) {
+            await this.userOnlyScripts[i]();
+        }
+    };
+
+    let interactionListener = () => {
+        stopListening();
+        initializeUserOnlyScripts();
+    }
+
+    let startListening = () => {
+        document.addEventListener('mousemove', interactionListener);
+        document.addEventListener('touchmove', interactionListener);
+        document.addEventListener('click', interactionListener);
+        document.addEventListener('scroll', interactionListener);
+    };
+
+    let stopListening = () => {
+        document.removeEventListener('mousemove', interactionListener);
+        document.removeEventListener('touchmove', interactionListener);
+        document.removeEventListener('click', interactionListener);
+        document.removeEventListener('scroll', interactionListener);
+    };
+
+    this.afterInit = async (l) => {
+        if (this.initialized) {
+            if (afterInitInProgress) {
+                afterInitQueue.push(l);
+            } else {
+                afterInitInProgress = true;
+                await l();
+                while (afterInitQueue.length > 0) {
+                    cb = afterInitInProgress.shift();
+                    await cb();
+                }
+                afterInitInProgress = false;
+            }
+            return;
+        }
+        this.listeners.push(l);
+    };
+  
+    this.deferredStyle = (s) => {
+        this.afterInit(function(){
+            var styles = document.createElement('link');
+            styles.rel = 'stylesheet';
+            styles.type = 'text/css';
+            styles.media = 'all';
+            styles.href = s;
+            document.getElementsByTagName('head')[0].appendChild(styles);
+        });
+    };
+  
+    this.deferredScript = (s) => {
+        const method = 'afterInit';
+        this[method](async function(){
+            if (typeof(s) === 'string') {
+                await (new Promise((resolve) => {
+                    var script = document.createElement('script');
+                    script.onload = resolve;
+                    script.src = s;
+                    document.getElementsByTagName('head')[0].appendChild(script);
+                }));
+            } else {
+                await s();
+            }
+        });
+    };
+
+    this.enableTimeout = (t) => {
+        initTimeout = (t ? t : 1000);
+    };
+
+    this.disableTimeout = () => {
+        initTimeout = null;
+    }
+
+    this.enableCookie = (n) => {
+        cookieName = (n ? n : 'pst_skip_delay');
+        if (getCookie(cookieName)) {
+            initializeUserOnlyScripts();
+        }
+        setCookie(cookieName, 1, 30);
+    };
+
+    window.addEventListener('load', () => {
+        startListening();
+        if (initTimeout) {
+            setTimeout(initialize, initTimeout);
+        }
+    });
 };
